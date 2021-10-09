09fa0e43a8c19da9a90e46e383a2b67dbfe2946c
AdabalaDivya <67156454+AdabalaDivya@users.noreply.github.com>
2021-10-09 18:45:41 +0530

    Add files via upload

---
+++ SecurityQuiz/src/test/java/com/quiz/security/SecurityQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiz.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
